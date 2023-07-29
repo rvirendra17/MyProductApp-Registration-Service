c3a5f9068c46d2d75cda01940bc748ceb2554b23
Virendra Rajput <virendrarajput1988@gmail.com>
2023-07-29 16:43:24 +0530

    Initial commit

---
+++ src/main/java/com/in/myproduct/registrationservice/RegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.in.myproduct.registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
+
+}
